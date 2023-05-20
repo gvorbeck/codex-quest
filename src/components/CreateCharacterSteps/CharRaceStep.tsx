368471c96807107e543f561910c588f3506e0083
gvorbeck <gvorbeck@gmail.com>
2023-05-20 10:34:23 -0400

    clear classes on race change

--- src/components/CreateCharacterSteps/CharRaceStep.tsx
+++ src/components/CreateCharacterSteps/CharRaceStep.tsx
@@ -14,15 +14,24 @@ type CharRaceStepProps = {
   };
   race: string;
   setRace: (race: string) => void;
+  setPlayerClass: (playerClass: string) => void;
+  setComboClass: (comboClass: boolean) => void;
+  setCheckedClasses: (checkedClasses: string[]) => void;
 };
 
 export default function CharRaceStep({
   abilities,
   race,
   setRace,
+  setPlayerClass,
+  setComboClass,
+  setCheckedClasses,
 }: CharRaceStepProps) {
   const onChange = (e: RadioChangeEvent) => {
     setRace(e.target.value);
+    setPlayerClass("");
+    setComboClass(false);
+    setCheckedClasses([]);
   };
 
   return (
