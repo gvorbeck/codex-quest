a7146cf3f543333e5c24554a2cef546d7196d016
gvorbeck <gvorbeck@gmail.com>
2023-05-21 23:56:07 -0400

    hit points init

--- src/components/CreateCharacterSteps/CharHitPointsStep.tsx
+++ src/components/CreateCharacterSteps/CharHitPointsStep.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Button, InputNumber, Space } from "antd";
+import { DiceRoller } from "@dice-roller/rpg-dice-roller";
 
 type CharHitPointsStepProps = {
   hitPoints: number;
@@ -28,12 +30,60 @@ export default function CharHitPointsStep({
   hitDice,
   setHitDice,
 }: CharHitPointsStepProps) {
+  const roller = new DiceRoller();
+
+  useEffect(() => {
+    let dice: string;
+
+    if (playerClass === "Cleric") {
+      dice = "d6";
+    } else if (playerClass === "Fighter") {
+      dice = "d8";
+      if (race === "Elf" || race === "Halfling") {
+        dice = "d6";
+      }
+    } else if (playerClass === "Magic-User") {
+      dice = "d4";
+    } else {
+      // Thief
+      dice = "d4";
+    }
+
+    setHitDice(dice);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onClick = () => {
+    const result = roller.roll(hitDice);
+    if (!(result instanceof Array)) handleHitPointValue(result.total);
+  };
+
+  const handleHitPointValue = (value: number | null) => {
+    if (value === null) return;
+    value += parseInt(constitutionModifier);
+    if (value < 1) value = 1;
+    setHitPoints(value);
+  };
+
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.select();
+  };
+
   return (
     <Space.Compact>
-      <InputNumber />
-      <Button type="primary" onClick={() => console.log("boop")}>
-        Roll 3d6
-      </Button>
+      <InputNumber
+        max={11}
+        min={1}
+        defaultValue={0}
+        onChange={handleHitPointValue}
+        onFocus={handleFocus}
+        type="number"
+        value={hitPoints}
+      />
+      <Button
+        type="primary"
+        onClick={onClick}
+      >{`Roll 1${hitDice}${constitutionModifier}`}</Button>
     </Space.Compact>
   );
 }
