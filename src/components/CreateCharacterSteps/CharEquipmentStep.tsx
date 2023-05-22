c87072fb7a141f45bf0dd41a2576c1907ce78bc8
Garrett Vorbeck <gvorbeck@gmail.com>
2023-05-22 11:37:44 -0400

    weapon collection queries

--- src/components/CreateCharacterSteps/CharEquipmentStep.tsx
+++ src/components/CreateCharacterSteps/CharEquipmentStep.tsx
@@ -6,15 +6,18 @@ interface Weapon {
   costCurrency: string;
   costValue: number;
   name: string;
-  size: string;
+  size?: string;
   weight: number;
+  damage?: string;
 }
 
-interface Axe extends Weapon {
-  damage: string;
-}
-
-interface Bow extends Weapon {}
+type Axe = Weapon;
+type Bow = Weapon;
+type Dagger = Weapon;
+type Sword = Weapon;
+type HammerMace = Weapon;
+type OtherWeapon = Weapon;
+type Ammunition = Weapon;
 
 type CharEquipmentStepProps = {
   gold: number;
@@ -31,6 +34,11 @@ export default function CharEquipmentStep({
 }: CharEquipmentStepProps) {
   const [axes, setAxes] = useState<Axe[]>([]);
   const [bows, setBows] = useState<Bow[]>([]);
+  const [daggers, setDaggers] = useState<Dagger[]>([]);
+  const [swords, setSwords] = useState<Sword[]>([]);
+  const [hammersMaces, setHammersMaces] = useState<HammerMace[]>([]);
+  const [otherWeapons, setOtherWeapons] = useState<OtherWeapon[]>([]);
+  const [ammunition, setAmmunition] = useState<Ammunition[]>([]);
 
   useEffect(() => {
     const fetchAxes = async () => {
@@ -47,7 +55,58 @@ export default function CharEquipmentStep({
       setBows(bowsArray);
     };
 
-    Promise.all([fetchAxes(), fetchBows()])
+    const fetchDaggers = async () => {
+      const daggersCollection = collection(db, "daggers");
+      const daggersSnapshot = await getDocs(daggersCollection);
+      const daggersArray = daggersSnapshot.docs.map(
+        (doc) => doc.data() as Dagger
+      );
+      setDaggers(daggersArray);
+    };
+
+    const fetchSwords = async () => {
+      const swordsCollection = collection(db, "swords");
+      const swordsSnapshot = await getDocs(swordsCollection);
+      const swordsArray = swordsSnapshot.docs.map((doc) => doc.data() as Sword);
+      setSwords(swordsArray);
+    };
+
+    const fetchHammerMaces = async () => {
+      const hammersMacesCollection = collection(db, "hammers-maces");
+      const hammersMacesSnapshot = await getDocs(hammersMacesCollection);
+      const hammersMacesArray = hammersMacesSnapshot.docs.map(
+        (doc) => doc.data() as HammerMace
+      );
+      setHammersMaces(hammersMacesArray);
+    };
+
+    const fetchOtherWeapons = async () => {
+      const otherWepaonsCollection = collection(db, "other-weapons");
+      const otherWepaonsSnapshot = await getDocs(otherWepaonsCollection);
+      const otherWepaonsArray = otherWepaonsSnapshot.docs.map(
+        (doc) => doc.data() as OtherWeapon
+      );
+      setOtherWeapons(otherWepaonsArray);
+    };
+
+    const fetchAmmunition = async () => {
+      const ammunitionCollection = collection(db, "ammunition");
+      const ammunitionSnapshot = await getDocs(ammunitionCollection);
+      const ammunitionArray = ammunitionSnapshot.docs.map(
+        (doc) => doc.data() as Ammunition
+      );
+      setAmmunition(ammunitionArray);
+    };
+
+    Promise.all([
+      fetchAxes(),
+      fetchBows(),
+      fetchDaggers(),
+      fetchSwords(),
+      fetchHammerMaces(),
+      fetchOtherWeapons(),
+      fetchAmmunition(),
+    ])
       .then(() => console.log("Data fetched from all collections"))
       .catch((error) =>
         console.error("Error fetching data from collections", error)
