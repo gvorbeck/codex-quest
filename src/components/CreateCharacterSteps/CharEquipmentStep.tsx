7eaccf361c12aff79782f83247011ddcbdd62be5
Garrett Vorbeck <gvorbeck@gmail.com>
2023-05-22 13:33:49 -0400

    weapons categories

--- src/components/CreateCharacterSteps/CharEquipmentStep.tsx
+++ src/components/CreateCharacterSteps/CharEquipmentStep.tsx
@@ -1,23 +1,20 @@
-import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
 import { db } from "../../firebase";
-import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { Button, Collapse, InputNumber, Space, Spin, Typography } from "antd";
+import { DiceRoller } from "@dice-roller/rpg-dice-roller";
 
-interface Weapon {
+interface Item {
   costCurrency: string;
   costValue: number;
   name: string;
-  size?: string;
   weight: number;
-  damage?: string;
 }
 
-type Axe = Weapon;
-type Bow = Weapon;
-type Dagger = Weapon;
-type Sword = Weapon;
-type HammerMace = Weapon;
-type OtherWeapon = Weapon;
-type Ammunition = Weapon;
+interface Weapon extends Item {
+  size?: string;
+  damage?: string;
+}
 
 type CharEquipmentStepProps = {
   gold: number;
@@ -26,94 +23,100 @@ type CharEquipmentStepProps = {
   setEquipment: (equipment: {}) => void;
 };
 
+const { Panel } = Collapse;
+const { Paragraph, Title } = Typography;
+
 export default function CharEquipmentStep({
   gold,
   equipment,
   setGold,
   setEquipment,
 }: CharEquipmentStepProps) {
-  const [axes, setAxes] = useState<Axe[]>([]);
-  const [bows, setBows] = useState<Bow[]>([]);
-  const [daggers, setDaggers] = useState<Dagger[]>([]);
-  const [swords, setSwords] = useState<Sword[]>([]);
-  const [hammersMaces, setHammersMaces] = useState<HammerMace[]>([]);
-  const [otherWeapons, setOtherWeapons] = useState<OtherWeapon[]>([]);
-  const [ammunition, setAmmunition] = useState<Ammunition[]>([]);
+  const [axes, setAxes] = useState<Weapon[]>([]);
+  const [bows, setBows] = useState<Weapon[]>([]);
+  const [daggers, setDaggers] = useState<Weapon[]>([]);
+  const [swords, setSwords] = useState<Weapon[]>([]);
+  const [hammersMaces, setHammersMaces] = useState<Weapon[]>([]);
+  const [otherWeapons, setOtherWeapons] = useState<Weapon[]>([]);
+  const [ammunition, setAmmunition] = useState<Weapon[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const weaponsCollectionMap = {
+    axes: setAxes,
+    bows: setBows,
+    daggers: setDaggers,
+    swords: setSwords,
+    hammersMaces: setHammersMaces,
+    otherWeapons: setOtherWeapons,
+    ammunition: setAmmunition,
+  };
+
+  const weaponsRef = useRef<Record<string, Weapon[]>>({});
+
+  const roller = new DiceRoller();
+  const rollStartingGold = () => {
+    const result = roller.roll("3d6*10");
+    if (!(result instanceof Array) && result.total !== null)
+      updateStartingGold(result.total);
+  };
+  const updateStartingGold = (startingGold: number | null) =>
+    startingGold !== null && setGold(startingGold);
+
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.select();
+  };
 
   useEffect(() => {
-    const fetchAxes = async () => {
-      const axesCollection = collection(db, "axes");
-      const axesSnapshot = await getDocs(axesCollection);
-      const axesArray = axesSnapshot.docs.map((doc) => doc.data() as Axe);
-      setAxes(axesArray);
-    };
+    const fetchCollections = Object.entries(weaponsCollectionMap).map(
+      async ([collectionName, setStateFunc]) => {
+        const coll = collection(db, collectionName);
+        const snapshot = await getDocs(coll);
+        const dataArray = snapshot.docs.map((doc) => doc.data() as Weapon);
+        setStateFunc(dataArray);
+        weaponsRef.current[collectionName] = dataArray; // Adding data to weapons object
+      }
+    );
 
-    const fetchBows = async () => {
-      const bowsCollection = collection(db, "bows");
-      const bowsSnapshot = await getDocs(bowsCollection);
-      const bowsArray = bowsSnapshot.docs.map((doc) => doc.data() as Bow);
-      setBows(bowsArray);
-    };
-
-    const fetchDaggers = async () => {
-      const daggersCollection = collection(db, "daggers");
-      const daggersSnapshot = await getDocs(daggersCollection);
-      const daggersArray = daggersSnapshot.docs.map(
-        (doc) => doc.data() as Dagger
-      );
-      setDaggers(daggersArray);
-    };
-
-    const fetchSwords = async () => {
-      const swordsCollection = collection(db, "swords");
-      const swordsSnapshot = await getDocs(swordsCollection);
-      const swordsArray = swordsSnapshot.docs.map((doc) => doc.data() as Sword);
-      setSwords(swordsArray);
-    };
-
-    const fetchHammerMaces = async () => {
-      const hammersMacesCollection = collection(db, "hammers-maces");
-      const hammersMacesSnapshot = await getDocs(hammersMacesCollection);
-      const hammersMacesArray = hammersMacesSnapshot.docs.map(
-        (doc) => doc.data() as HammerMace
-      );
-      setHammersMaces(hammersMacesArray);
-    };
-
-    const fetchOtherWeapons = async () => {
-      const otherWepaonsCollection = collection(db, "other-weapons");
-      const otherWepaonsSnapshot = await getDocs(otherWepaonsCollection);
-      const otherWepaonsArray = otherWepaonsSnapshot.docs.map(
-        (doc) => doc.data() as OtherWeapon
-      );
-      setOtherWeapons(otherWepaonsArray);
-    };
-
-    const fetchAmmunition = async () => {
-      const ammunitionCollection = collection(db, "ammunition");
-      const ammunitionSnapshot = await getDocs(ammunitionCollection);
-      const ammunitionArray = ammunitionSnapshot.docs.map(
-        (doc) => doc.data() as Ammunition
-      );
-      setAmmunition(ammunitionArray);
-    };
-
-    Promise.all([
-      fetchAxes(),
-      fetchBows(),
-      fetchDaggers(),
-      fetchSwords(),
-      fetchHammerMaces(),
-      fetchOtherWeapons(),
-      fetchAmmunition(),
-    ])
-      .then(() => console.log("Data fetched from all collections"))
+    Promise.all(fetchCollections)
+      .then(() => setIsLoading(false))
       .catch((error) =>
         console.error("Error fetching data from collections", error)
       );
+
+    console.log(weaponsRef.current);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // useEffect(() => console.log("foo", axes), [axes]);
+  if (isLoading) {
+    return <Spin />;
+  }
 
-  return <div />;
+  return (
+    <>
+      <Space.Compact>
+        <InputNumber
+          max={180}
+          min={30}
+          defaultValue={0}
+          onChange={updateStartingGold}
+          onFocus={handleFocus}
+          type="number"
+          value={gold}
+        />
+        <Button
+          type="primary"
+          onClick={rollStartingGold}
+        >{`Roll 3d6x10`}</Button>
+      </Space.Compact>
+      <Title level={2}>Weapons</Title>
+      <Collapse accordion>
+        {Object.entries(weaponsRef.current).map(([key, value]) => (
+          <Panel header={key.charAt(0).toUpperCase() + key.slice(1)} key={key}>
+            <Paragraph>bar</Paragraph>
+          </Panel>
+        ))}
+      </Collapse>
+    </>
+  );
 }
