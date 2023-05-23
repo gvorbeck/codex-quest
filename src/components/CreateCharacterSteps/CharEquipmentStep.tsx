76b82acff47c59af64170b3911c5c51be3bdec3e
gvorbeck <gvorbeck@gmail.com>
2023-05-22 22:35:04 -0400

    cleanup

--- src/components/CreateCharacterSteps/CharEquipmentStep.tsx
+++ src/components/CreateCharacterSteps/CharEquipmentStep.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useRef, useState } from "react";
 import { db } from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
