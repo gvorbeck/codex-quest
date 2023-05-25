1668da5b97de237ac29949f14a02c652cc5248d1
Garrett Vorbeck <gvorbeck@gmail.com>
2023-05-25 16:45:14 -0400

    exhaustive deps

--- src/components/CreateCharacterSteps/CharEquipmentStep.tsx
+++ src/components/CreateCharacterSteps/CharEquipmentStep.tsx
@@ -111,6 +111,7 @@ export default function CharEquipmentStep({
     fetchEquipment();
 
     console.log(race, strength, weightRestrictions);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   /**
