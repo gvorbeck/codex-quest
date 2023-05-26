a5ee51cf8e86b6db4c8fdf7057691e2a69a6d30e
Garrett Vorbeck <gvorbeck@gmail.com>
2023-05-26 13:29:53 -0400

    building out upload

--- src/components/CreateCharacterSteps/CharNameStep.tsx
+++ src/components/CreateCharacterSteps/CharNameStep.tsx
@@ -1,73 +1,75 @@
-import { Input, message, Upload } from "antd";
+import React, { ChangeEvent, useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Input, Modal, Upload } from "antd";
+import type { RcFile, UploadProps } from "antd/es/upload";
+import type { UploadFile } from "antd/es/upload/interface";
 import { CharNameStepProps } from "../types";
-import { UploadOutlined } from "@ant-design/icons";
-import { storage } from "../../firebase";
-import { UploadTaskSnapshot } from "firebase/storage";
-import type { UploadProps } from "antd";
+// import { storage } from "../../firebase";
+// import { UploadTaskSnapshot } from "firebase/storage";
 
-// function beforeUpload(file: any) {
-//   const isJpgOrPng =
-//     file.type === "image/jpeg" ||
-//     file.type === "image/png" ||
-//     file.type === "image/gif";
-//   if (!isJpgOrPng) {
-//     message.error("You can only upload JPG/PNG/GIF file!");
-//   }
-//   const isLt2M = file.size / 1024 / 1024 < 2;
-//   if (!isLt2M) {
-//     message.error("Image must smaller than 2MB!");
-//   }
-//   return isJpgOrPng && isLt2M;
-// }
-
-// function handleUpload(options: UploadRequestOption) {
-//   const { file, onSuccess, onError, onProgress } = options;
-//   const uploadTask = storage.ref(`images/${file.name}`).put(file);
-
-//   uploadTask.on(
-//     "state_changed",
-//     (snapshot: UploadTaskSnapshot) => {
-//       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-//       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//       console.log("Upload is " + progress + "% done");
-//       onProgress({ percent: progress }, file);
-//     },
-//     (error: Error) => {
-//       // Handle unsuccessful uploads
-//       console.log(error);
-//       onError(error);
-//     },
-//     async () => {
-//       // Handle successful uploads on complete
-//       try {
-//         const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
-//         console.log("File available at", downloadURL);
-//         onSuccess({ downloadURL }, file);
-//       } catch (error) {
-//         onError(error);
-//       }
-//     }
-//   );
-// }
+const getBase64 = (file: RcFile): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
 
 export default function CharNameStep({ name, setName }: CharNameStepProps) {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newName = event.target.value;
+    setName(newName);
+  };
+
+  const handleCancel = () => setPreviewOpen(false);
+
+  const handlePreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as RcFile);
+    }
+
+    setPreviewImage(file.url || (file.preview as string));
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name || file.url!.substring(file.url!.lastIndexOf("/") + 1)
+    );
+  };
+
+  const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) =>
+    setFileList(newFileList);
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
   return (
     <>
-      <Input />
-      {/* <Upload
-        name="avatar"
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76" // You can remove this when using Firebase Storage
-        beforeUpload={beforeUpload}
-        customRequest={handleUpload}
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={handleChange}
       >
-        <div>
-          <UploadOutlined />
-          <div style={{ marginTop: 8 }}>Upload</div>
-        </div>
-      </Upload> */}
+        {fileList.length >= 1 ? null : uploadButton}
+      </Upload>
+      <Modal
+        open={previewOpen}
+        title={previewTitle}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        <img alt="example" style={{ width: "100%" }} src={previewImage} />
+      </Modal>
+      <Input value={name} onChange={handleNameChange} />
     </>
   );
 }
