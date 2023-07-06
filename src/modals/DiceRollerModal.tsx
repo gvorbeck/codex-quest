96343303edc914eaa69f8477017bdb4be2393c75
Garrett Vorbeck <gvorbeck@gmail.com>
2023-07-05 21:34:43 -0400

    rethinking

--- src/modals/DiceRollerModal.tsx
+++ src/modals/DiceRollerModal.tsx
@@ -12,6 +12,7 @@ export default function DiceRollerModal({
   handleCancel,
 }: DiceRollerModalProps) {
   const [diceFormula, setDiceFormula] = useState<[number, number][]>([]);
+  const [inputValue, setInputValue] = useState("");
   const inputRef = useRef<InputRef>(null);
 
   const handleContextMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -73,6 +74,7 @@ export default function DiceRollerModal({
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
+    setInputValue(event.target.value);
   };
 
   return (
@@ -88,23 +90,24 @@ export default function DiceRollerModal({
         <Input
           ref={inputRef}
           placeholder="Enter a dice formula to roll"
-          value={diceFormula
-            .map(([count, type]) => `${count}d${type}`)
-            .join("+")}
+          // value={diceFormula
+          //   .map(([count, type]) => `${count}d${type}`)
+          //   .join("+")}
+          value={inputValue}
           onChange={handleInputChange}
         />
         <Button type="primary" onClick={handleRollClick}>
           Roll
         </Button>
       </Space.Compact>
-      <div className="flex justify-between my-4">
+      {/* <div className="flex justify-between my-4">
         <DiceButton die="d4" />
         <DiceButton die="d6" />
         <DiceButton die="d8" />
         <DiceButton die="d10" />
         <DiceButton die="d12" />
         <DiceButton die="d20" />
-      </div>
+      </div> */}
       <Divider />
       <Typography.Paragraph type="secondary">
         Click to add a die to the formula. Right click to remove a die.
