b2646f79cf5bdb6c620cf68f2b79e401f4a29580
gvorbeck <gvorbeck@gmail.com>
2023-07-11 19:29:17 -0400

    cleanup

--- src/components/CharacterSheet/SimpleNumberStat.tsx
+++ src/components/CharacterSheet/SimpleNumberStat.tsx
@@ -1,22 +1,25 @@
-import { Typography } from "antd";
+import { Statistic, Typography } from "antd";
 import { SimpleNumberStatProps } from "../types";
 
 export default function SimpleNumberStat({
   title,
   value,
 }: SimpleNumberStatProps) {
-  // Prevent "Hit Dice" text from wrapping
-  let className = value.toString().length > 3 ? "md:text-4xl lg:text-5xl" : "";
   return (
-    <div className="text-center">
-      <Typography.Title level={3} className="mt-0 mb-2 !text-shipGray">
-        {title}
-      </Typography.Title>
-      <Typography.Text
-        className={`${className} text-6xl font-bold text-shipGray`}
-      >
-        {value}
-      </Typography.Text>
-    </div>
+    <Statistic
+      className="text-center"
+      title={
+        <Typography.Title level={3} className="mt-0 mb-2 text-shipGray">
+          {title}
+        </Typography.Title>
+      }
+      value={value}
+      valueStyle={{
+        fontSize: "3.75rem",
+        lineHeight: "1",
+        fontWeight: "bold",
+        color: "#3E3643",
+      }}
+    />
   );
 }
