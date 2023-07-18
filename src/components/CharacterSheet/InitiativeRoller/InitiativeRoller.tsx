94fb16cdc2032629009854baf93f2c5319a0bd8a
Garrett Vorbeck <gvorbeck@gmail.com>
2023-07-18 14:00:53 -0400

    context fix for initiative roller

--- src/components/CharacterSheet/InitiativeRoller/InitiativeRoller.tsx
+++ src/components/CharacterSheet/InitiativeRoller/InitiativeRoller.tsx
@@ -2,22 +2,26 @@ import { Button, Tooltip, notification } from "antd";
 import { DiceRoller } from "@dice-roller/rpg-dice-roller";
 import { InitiativeRollerProps } from "./definitions";
 
-const openNotification = (result: number) => {
-  notification.open({
-    message: "Initiative Roll",
-    description: result,
-    duration: 0,
-    className: "!bg-seaBuckthorn",
-  });
-};
-
 export default function InitiativeRoller({
   characterData,
 }: InitiativeRollerProps) {
+  const [api, contextHolder] = notification.useNotification();
+
+  const openNotification = (result: number) => {
+    api.open({
+      message: "Initiative Roll",
+      description: result,
+      duration: 0,
+      className: "!bg-seaBuckthorn",
+    });
+  };
+
   const rollTooltip = `1d6 + DEX modifier ${
     characterData.race === "Halfling" ? "+ 1 as a Halfling" : ""
   }`;
+
   const roller = new DiceRoller();
+
   const rollInitiative = () => {
     let result = roller.roll(
       `1d6${characterData.abilities.modifiers.dexterity}${
@@ -27,11 +31,15 @@ export default function InitiativeRoller({
     if (result.total === 0) result = 1;
     openNotification(result.output);
   };
+
   return (
-    <Tooltip title={rollTooltip}>
-      <Button type="primary" onClick={rollInitiative}>
-        Roll Initiative
-      </Button>
-    </Tooltip>
+    <>
+      {contextHolder}
+      <Tooltip title={rollTooltip}>
+        <Button type="primary" onClick={rollInitiative}>
+          Roll Initiative
+        </Button>
+      </Tooltip>
+    </>
   );
 }
