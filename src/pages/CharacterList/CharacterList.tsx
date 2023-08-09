72a6ba5dcf2512bf3ab3e35d8380885d0a64d5f8
Garrett Vorbeck <gvorbeck@gmail.com>
2023-08-09 17:21:46 -0400

    sorting and presentation

--- src/pages/CharacterList/CharacterList.tsx
+++ src/pages/CharacterList/CharacterList.tsx
@@ -39,72 +39,79 @@ export default function CharacterList({
         <Spin />
       ) : characters.length ? (
         <Row justify={"start"} gutter={32} className="gap-y-9">
-          {characters.map((character) => (
-            <Col xs={24} md={12} lg={6} key={character.id}>
-              <Card
-                bodyStyle={{
-                  backgroundColor: "#e2e8f0",
-                  borderRadius: "0.5rem 0.5rem 0 0",
-                  borderBottom: "0.25rem solid rgba(62,53,67, 0.15)",
-                }}
-                actions={[
-                  <SolutionOutlined
-                    key="sheet"
-                    onClick={() =>
-                      navigate(`/u/${user?.uid}/c/${character.id}`)
+          {characters
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((characterData) => (
+              <Col xs={24} md={12} lg={6} key={characterData.id}>
+                <Card
+                  bodyStyle={{
+                    backgroundColor: "#e2e8f0",
+                    borderRadius: "0.5rem 0.5rem 0 0",
+                    border: "1px solid rgba(62,53,67, 0.15)",
+                    borderBottomWidth: "2px",
+                  }}
+                  actions={[
+                    <SolutionOutlined
+                      key="sheet"
+                      onClick={() =>
+                        navigate(`/u/${user?.uid}/c/${characterData.id}`)
+                      }
+                      title="Go to Character Sheet"
+                      aria-label="Go to Character Sheet"
+                    />,
+                    <Popconfirm
+                      title="Delete this character?"
+                      description={`This cannot be undone!`}
+                      onConfirm={() =>
+                        characterData?.id && confirm(characterData.id)
+                      }
+                      okText="Yes"
+                      cancelText="No"
+                    >
+                      <DeleteOutlined
+                        key="delete"
+                        aria-label="Delete character"
+                        title="Delete character"
+                      />
+                    </Popconfirm>,
+                  ]}
+                  className="h-full flex flex-col justify-between [&>ul]:border-t-solid [&>ul]:border-t-2 [&>ul]:border-[rgba(0,0,0,0.1)]"
+                >
+                  <Card.Meta
+                    description={
+                      <div className="flex flex-col items-center text-slate-600">
+                        <span>Level {characterData.level}</span>
+                        <span>{characterData.race}</span>
+                        <span>{characterData.class}</span>
+                      </div>
                     }
-                    title="Go to Character Sheet"
-                    aria-label="Go to Character Sheet"
-                  />,
-                  <Popconfirm
-                    title="Delete this character?"
-                    description={`This cannot be undone!`}
-                    onConfirm={() => character?.id && confirm(character.id)}
-                    okText="Yes"
-                    cancelText="No"
-                  >
-                    <DeleteOutlined
-                      key="delete"
-                      aria-label="Delete character"
-                      title="Delete character"
-                    />
-                  </Popconfirm>,
-                ]}
-                className="h-full flex flex-col justify-between"
-              >
-                <Card.Meta
-                  description={
-                    <div className="flex flex-col items-center text-slate-600">
-                      <span>Level {character.level}</span>
-                      <span>{character.race}</span>
-                      <span>{character.class}</span>
-                    </div>
-                  }
-                  className="flex-col [&>.ant-card-meta-avatar]:self-center [&>.ant-card-meta-avatar]:pr-0 [&>.ant-card-meta-avatar]:mb-2 [&>.ant-card-meta-avatar]:text-springWood"
-                  avatar={
-                    character.avatar ? (
-                      <Avatar
-                        size={64}
-                        src={character.avatar}
-                        alt={character.name}
-                        className="shadow-md"
-                      />
-                    ) : (
-                      <Avatar
-                        size={64}
-                        icon={<UserOutlined />}
-                        alt={character.name}
-                        className="shadow-md"
-                      />
-                    )
-                  }
-                  title={
-                    <span className="text-center block">{character.name}</span>
-                  }
-                />
-              </Card>
-            </Col>
-          ))}
+                    className="flex-col [&>.ant-card-meta-avatar]:self-center [&>.ant-card-meta-avatar]:pr-0 [&>.ant-card-meta-avatar]:mb-2 [&>.ant-card-meta-avatar]:text-springWood"
+                    avatar={
+                      characterData.avatar ? (
+                        <Avatar
+                          size={64}
+                          src={characterData.avatar}
+                          alt={characterData.name}
+                          className="shadow-md border-solid border-2 border-seaBuckthorn"
+                        />
+                      ) : (
+                        <Avatar
+                          size={64}
+                          icon={<UserOutlined />}
+                          alt={characterData.name}
+                          className="shadow-md border-solid border-2 border-seaBuckthorn"
+                        />
+                      )
+                    }
+                    title={
+                      <span className="text-center block">
+                        {characterData.name}
+                      </span>
+                    }
+                  />
+                </Card>
+              </Col>
+            ))}
         </Row>
       ) : (
         <Empty description="Create your first character by clicking the button above." />
