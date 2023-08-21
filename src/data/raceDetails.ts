f626f32fe16cb096a950e57ba15c73b3c2bd91dd
Garrett Vorbeck <gvorbeck@gmail.com>
2023-08-21 14:32:13 -0400

    Gnome restrictions

--- src/data/raceDetails.ts
+++ src/data/raceDetails.ts
@@ -44,11 +44,7 @@ export const raceDetails: Record<
     description: marked(
       `Gnomes are small and stocky, more so than halflings but not as much as dwarves; both male and female gnomes stand around three and a half feet tall and typically weigh around 90 pounds. The most noticeable features about a gnome from the standpoint of other races is their pointed ears and noses. They are renowned for their rapidly-changing moods, sometimes gruff and contrary, sometimes whimsical and humorous. They have a lifespan between two and three centuries long.`
     ),
-    restrictions: [
-      marked(`All **Gnomes** have Darkvision with a 30-foot range.`),
-      marked(
-        `When attacked in melee by creatures larger than man-sized, **Gnomes** gain a +1 bonus to their Armor Class.`
-      ),
+    specials: [
       marked(
         `**Gnomes** are naturally very observant; being smaller than most other races has made them cautious and aware of their surroundings. As a consequence, a Gnome has a 10% chance to detect an invisible or hidden creature within a 30 foot radius. This ability does not apply to inanimate objects such as secret doors or invisible objects.`
       ),
@@ -56,7 +52,18 @@ export const raceDetails: Record<
         `When fighting an invisible opponent, a **Gnome** who has successfully detected the invisible creature suffers only a -2 penalty on the attack roll, rather than the usual -4 penalty as given `
       ),
     ],
-    specials: [marked(`foo`)],
+    restrictions: [
+      marked(
+        `**Gnomes** may become Clerics, Fighters, Magic-Users, Thieves, or Magic-User/Thieves. They are required to have a minimum Constitution of 9.`
+      ),
+      marked(
+        `Due to their small stature, **Gnomes** may not have a Strength higher than 17.`
+      ),
+      marked(
+        `**Gnomes** never roll larger than six-sided dice (d6) for hit points regardless of class.`
+      ),
+      marked(`**Gnomes** may not employ Large weapons.`),
+    ],
   },
   [RaceNames.HALFLING]: {
     description: marked(
