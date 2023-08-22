11a8038fa5a9b3ca68e21980dea2d80134adbd8f
Garrett Vorbeck <gvorbeck@gmail.com>
2023-08-22 17:23:11 -0400

    consistant prop naming

--- src/components/CharacterSheet/BaseStats/ExperiencePoints/definitions.ts
+++ src/components/CharacterSheet/BaseStats/ExperiencePoints/definitions.ts
@@ -1,8 +1,8 @@
 import { CharacterData } from "../../../definitions";
 
 export interface ExperiencePointsProps {
-  character: CharacterData;
-  setCharacter?: (character: CharacterData) => void;
+  characterData: CharacterData;
+  setCharacterData?: (character: CharacterData) => void;
   className?: string;
   userIsOwner?: boolean;
   showLevelUpModal?: () => void;
