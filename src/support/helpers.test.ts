d27e3a80b6aba175b241ec679affd2398f28c52d
Garrett Vorbeck <gvorbeck@gmail.com>
2023-08-25 17:18:04 -0400

    helper testing

--- src/support/helpers.test.ts
+++ src/support/helpers.test.ts
@@ -1,6 +1,7 @@
-import { getClassType, isStandardRace } from "./helpers";
+import { getClassType, isStandardRace, getDisabledClasses } from "./helpers";
 import { ClassNamesTwo } from "../data/classes";
 import { RaceNamesTwo } from "../data/races";
+import { Abilities } from "../components/CreateCharacter/CharacterAbilities/definitions";
 
 // getClassType
 test('returns "standard" for a single standard class', () => {
@@ -28,4 +29,43 @@ test('returns "true" for a standard Race', () => {
 
 test('returns "false" for a non-standard Race', () => {
   expect(isStandardRace("Foo Bar")).toBe(false);
+});
+
+// getDisabledClasses
+const abilities = {
+  scores: {
+    strength: 9,
+    intelligence: 9,
+    wisdom: 9,
+    constitution: 9,
+    dexterity: 9,
+    charisma: 9,
+  },
+  modifiers: {
+    strength: "+0",
+    intelligence: "+0",
+    wisdom: "+0",
+    constitution: "+0",
+    dexterity: "+0",
+    charisma: "+0",
+  },
+};
+test(`returns "['Illusionist']"`, () => {
+  expect(
+    getDisabledClasses(RaceNamesTwo.HUMAN, abilities as Abilities)
+  ).toEqual(["Illusionist"]);
+});
+
+test(`returns "['Assassin', 'Barbarian', 'Illusionist', 'Magic-User']"`, () => {
+  expect(
+    getDisabledClasses(RaceNamesTwo.HALFLING, abilities as Abilities)
+  ).toEqual(["Assassin", "Barbarian", "Illusionist", "Magic-User"]);
+});
+
+test(`returns correct Classes`, () => {
+  expect(getDisabledClasses(RaceNamesTwo.ELF, abilities as Abilities)).toEqual([
+    "Assassin",
+    "Barbarian",
+    "Illusionist",
+  ]);
 });
