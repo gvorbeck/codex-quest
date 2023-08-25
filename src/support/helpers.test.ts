613bdd067e578cccc6bf64a72a41294fe8a19525
Garrett Vorbeck <gvorbeck@gmail.com>
2023-08-25 10:50:48 -0400

    more helper tests

--- src/support/helpers.test.ts
+++ src/support/helpers.test.ts
@@ -1,6 +1,8 @@
-import { getClassType } from "./helpers";
+import { getClassType, isStandardRace } from "./helpers";
 import { ClassNamesTwo } from "../data/classes";
+import { RaceNamesTwo } from "../data/races";
 
+// getClassType
 test('returns "standard" for a single standard class', () => {
   expect(getClassType(ClassNamesTwo.FIGHTER)).toBe("standard");
 });
@@ -17,4 +19,13 @@ test('returns "custom" for a class string with one standard class and a custom c
 
 test('returns "custom" for a class string with a completely custom class', () => {
   expect(getClassType("Foo")).toBe("custom");
+});
+
+// isStandardRace
+test('returns "true" for a standard Race', () => {
+  expect(isStandardRace(RaceNamesTwo.DWARF)).toBe(true);
+});
+
+test('returns "false" for a non-standard Race', () => {
+  expect(isStandardRace("Foo Bar")).toBe(false);
 });
