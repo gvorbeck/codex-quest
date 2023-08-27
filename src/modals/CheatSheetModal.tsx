f6ba864956b6a0fe3e8f199619021f0c51669c1e
gvorbeck <gvorbeck@gmail.com>
2023-08-27 00:40:58 -0400

    updates

--- src/modals/CheatSheetModal.tsx
+++ src/modals/CheatSheetModal.tsx
@@ -14,21 +14,6 @@ export default function CheatSheetModal({
       footer={false}
       closeIcon={<CloseIcon />}
     >
-      <Typography.Title level={3}>Combat</Typography.Title>
-      <List
-        size="small"
-        dataSource={[
-          "Check for Surprise (GM)",
-          "Check monster reaction (GM)",
-          "Combat cycle",
-        ]}
-        renderItem={(item, index) => (
-          <List.Item>
-            {index + 1}. {item}
-          </List.Item>
-        )}
-      />
-
       <Collapse className="bg-seaBuckthorn mt-4" accordion>
         <Collapse.Panel header="Surprise" key="1">
           <Typography.Paragraph>
@@ -42,6 +27,20 @@ export default function CheatSheetModal({
           </Typography.Paragraph>
         </Collapse.Panel>
         <Collapse.Panel header="Combat Overview" key="2">
+          <List
+            size="small"
+            dataSource={[
+              "Check for Surprise (GM)",
+              "Check monster reaction (GM)",
+              "Combat cycle",
+            ]}
+            renderItem={(item, index) => (
+              <List.Item>
+                {index + 1}. {item}
+              </List.Item>
+            )}
+          />
+          <Typography.Title level={4}>Combat Cycle</Typography.Title>
           <Typography.Paragraph>
             <strong>Roll Initiative</strong>. Roll 1d6 and add any bonuses or
             penalties, including DEX bonus, -1 if deafened, and -2 if blinded.
@@ -60,8 +59,67 @@ export default function CheatSheetModal({
             which they are casting a spell, the spell is spoiled and lost.
           </Typography.Paragraph>
         </Collapse.Panel>
+        <Collapse.Panel header="Combat Movement" key="3">
+          <Typography.Paragraph>
+            When opponents are over 5' apart, they move freely. Within 5',
+            they're "engaged" and follow Defensive Movement rules. Sometimes,
+            they may not know they're engaged, like during a sneak attack.
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            To evade, treat evasion and pursuit as combat actions. No mapping
+            while fleeing. Navigating obstacles may require a saving throw vs.
+            Death Ray. Failing means falling and pausing until the next round.
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            If pursuers get within 5', melee combat starts. Fleeing characters
+            risk "parting shots." If they escape the pursuer's sight for a full
+            round, they've evaded.
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            Turning your back on an adjacent, armed opponent allows them a
+            "parting shot" with a +2 attack bonus. You can also back away at
+            half speed while fighting, known as a fighting withdrawal. You still
+            move first.
+          </Typography.Paragraph>
+        </Collapse.Panel>
+        <Collapse.Panel header="Running" key="4">
+          <Typography.Paragraph>
+            A running character is not normally allowed to attack (except
+            Charging). Running characters can move at double their normal
+            encounter movement rate for a maximum number of rounds equal to 2
+            times the character's Constitution, after which they are exhausted
+            and may only walk (at the normal encounter rate). For monsters
+            without a given Constitution, allow the monster to run for 24
+            rounds. Exhausted characters or creatures must rest for at least a
+            turn before running again.
+          </Typography.Paragraph>
+        </Collapse.Panel>
+        <Collapse.Panel header="Charging" key="5">
+          <Typography.Paragraph>
+            In some cases, characters or creatures can attack after a running
+            move, known as a charge. The charger must move at least 10 feet, up
+            to double their normal rate, in a straight line toward the target.
+            The path must be clear, and the attacker should use a weapon like a
+            spear, lance, or polearm suitable for charging. Monsters with horns
+            can use natural attacks. If the attacker lacks line of sight at the
+            start, the opponent can't be charged.
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            The post-charge attack gets a +2 on the attack roll, but the charger
+            takes a -2 penalty to Armor Class for the round. If the attack hits,
+            it deals double damage.
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            <strong>Set Weapon Against Charge</strong>: Spears, pole arms, and
+            certain other piercing weapons deal double damage when “set” (braced
+            against the ground or floor) and used against a charging creature.
+            For this to be done, the character or creature being charged must
+            have equal or better Initiative; this counts as holding an action:
+            both attacker and defender act on the attacker's Initiative number
+            and are therefore simultaneous.
+          </Typography.Paragraph>
+        </Collapse.Panel>
       </Collapse>
-      <Typography.Title level={4}>Combat cycle</Typography.Title>
     </Modal>
   );
 }
