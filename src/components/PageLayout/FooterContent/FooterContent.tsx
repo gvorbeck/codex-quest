f51edb9848e411a8184c57fea898eef85ce892da
gvorbeck <gvorbeck@gmail.com>
2023-08-29 22:49:49 -0400

    cleric druid starting spell fix

--- src/components/PageLayout/FooterContent/FooterContent.tsx
+++ src/components/PageLayout/FooterContent/FooterContent.tsx
@@ -9,7 +9,7 @@ export default function FooterContent({ className }: FooterContentProps) {
   return (
     <div className={FooterContentClassNames}>
       <Typography.Paragraph>
-        © 2023 J. Garrett Vorbeck. All rights reserved. v1.11.1.1
+        © 2023 J. Garrett Vorbeck. All rights reserved. v1.11.1.2
       </Typography.Paragraph>
       <Typography.Paragraph>
         This site is based on the&nbsp;
