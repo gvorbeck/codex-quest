d82af415165f64483477539e64bc2f4e9242587c
gvorbeck <gvorbeck@gmail.com>
2023-09-17 10:10:02 -0400

    remove hardcoded values from footer

--- src/components/PageLayout/FooterContent/FooterContent.tsx
+++ src/components/PageLayout/FooterContent/FooterContent.tsx
@@ -3,7 +3,7 @@ import { Typography } from "antd";
 import { FooterContentProps } from "./definitions";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
-import { version } from "../../../../package.json";
+import { version, bfrpgRelease, bfrpgEdition } from "../../../../package.json";
 
 export default function FooterContent({ className }: FooterContentProps) {
   const FooterContentClassNames = classNames(
@@ -15,7 +15,8 @@ export default function FooterContent({ className }: FooterContentProps) {
   return (
     <div className={FooterContentClassNames}>
       <Typography.Paragraph>
-        © 2023 J. Garrett Vorbeck. All rights reserved. {version}
+        © {new Date().getFullYear()} J. Garrett Vorbeck. All rights reserved.{" "}
+        {version}
       </Typography.Paragraph>
       <Typography.Paragraph>
         This site is based on the&nbsp;
@@ -26,7 +27,7 @@ export default function FooterContent({ className }: FooterContentProps) {
         >
           Basic Fantasy Role-Playing Game created by Chris Gonnerman
         </a>{" "}
-        and is current to 4th Edition (release 135).
+        and is current to {bfrpgEdition} Edition (release {bfrpgRelease}).
       </Typography.Paragraph>
       <div className="grid grid-cols-2 md:flex md:gap-8 md:grid-rows-1">
         <Typography.Paragraph>
