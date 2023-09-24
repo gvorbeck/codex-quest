3f7455d1459bc46b6e5b5a88bc92b46b20466873
gvorbeck <gvorbeck@gmail.com>
2023-09-23 23:57:40 -0400

    tabs specials and restrictions

--- src/components/CharacterSheet/SpecialsRestrictions/SpecialsRestrictions.tsx
+++ src/components/CharacterSheet/SpecialsRestrictions/SpecialsRestrictions.tsx
@@ -1,9 +1,10 @@
-import { List, Typography } from "antd";
+import { List, Tabs, TabsProps, Typography } from "antd";
 import { marked } from "marked";
 import { SpecialsRestrictionsProps } from "./definitions";
 import { classes } from "../../../data/classes";
 import { races } from "../../../data/races";
 import { ClassNames, RaceNames } from "../../../data/definitions";
+import { titleCaseToCamelCase } from "../../../support/stringSupport";
 
 // Ant Design's List component treats the input as a string and not as HTML.
 // To render HTML, you need to use dangerouslySetInnerHTML prop in React.
@@ -13,46 +14,58 @@ const HtmlRender = ({ html }: { html: string }) => (
   <div dangerouslySetInnerHTML={{ __html: html }} />
 );
 
+const SpecialsRestrictionsList = ({ dataSource }: { dataSource: any[] }) => (
+  <List
+    bordered
+    dataSource={dataSource}
+    renderItem={(item) => (
+      <List.Item>
+        <HtmlRender html={marked(item)} />
+      </List.Item>
+    )}
+    className="print:border-0"
+    size="small"
+  />
+);
+
 export default function SpecialsRestrictions({
   characterData,
   className,
 }: SpecialsRestrictionsProps & React.ComponentPropsWithRef<"div">) {
-  // Gather the specials and restrictions for each class in the combination
-  const classSpecials: string[] = [];
-  const classRestrictions: string[] = [];
-  characterData.class.forEach((cls) => {
-    if (classes[cls as ClassNames]) {
-      classSpecials.push(
-        ...(classes[cls as ClassNames].details?.specials || [])
-      );
-      classRestrictions.push(
-        ...(classes[cls as ClassNames].details?.restrictions || [])
-      );
-    }
-  });
+  const items: TabsProps["items"] = [
+    {
+      key: titleCaseToCamelCase(characterData.race),
+      label: characterData.race,
+      children: (
+        <SpecialsRestrictionsList
+          dataSource={[
+            ...(races[characterData.race as RaceNames].details?.specials || []),
+            ...(races[characterData.race as RaceNames].details?.restrictions ||
+              []),
+          ]}
+        />
+      ),
+    },
+    ...characterData.class.map((cls) => ({
+      key: titleCaseToCamelCase(cls),
+      label: cls,
+      children: (
+        <SpecialsRestrictionsList
+          dataSource={[
+            ...(classes[cls as ClassNames].details?.specials || []),
+            ...(classes[cls as ClassNames].details?.restrictions || []),
+          ]}
+        />
+      ),
+    })),
+  ];
 
   return (
     <div className={className}>
       <Typography.Title level={3} className="mt-0 !text-shipGray">
         Special Abilities & Restrictions
       </Typography.Title>
-      <List
-        bordered
-        dataSource={[
-          ...(races[characterData.race as RaceNames].details?.specials || []),
-          ...classSpecials,
-          ...(races[characterData.race as RaceNames].details?.restrictions ||
-            []),
-          ...classRestrictions,
-        ]}
-        renderItem={(item) => (
-          <List.Item>
-            <HtmlRender html={marked(item)} />
-          </List.Item>
-        )}
-        className="print:border-0"
-        size="small"
-      />
+      <Tabs defaultActiveKey="1" items={items} />
     </div>
   );
 }
