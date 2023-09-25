01f6f60384d4fff0ac4477c15eb017725833e8c0
Garrett Vorbeck <gvorbeck@gmail.com>
2023-09-25 12:16:39 -0400

    initial faun

---
+++ src/data/races/faun.ts
@@ -0,0 +1,11 @@
+import { RaceSetup } from "./definitions";
+
+export const faun: RaceSetup = {
+  name: "Faun",
+  allowedStandardClasses: [],
+  details: {
+    description: "",
+    specials: [],
+    restrictions: [],
+  },
+};
