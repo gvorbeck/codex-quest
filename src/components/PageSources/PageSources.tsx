74f09e22f8b1e56c9611e5f6ed0dd756274fa10e
Garrett Vorbeck <gvorbeck@gmail.com>
2024-02-09 09:21:42 -0500

    Update release versions in PageSources.tsx

--- src/components/PageSources/PageSources.tsx
+++ src/components/PageSources/PageSources.tsx
@@ -17,63 +17,73 @@ const PageSources: React.FC<
       <List className="italic">
         <List.Item>
           Gonnerman, Chris. "Basic Fantasy Role-Playing Game Core Rules 4th
-          Edition." Basic Fantasy Role-Playing Game, 2023,
+          Edition." Release 137. Basic Fantasy Role-Playing Game, 2023,
           basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
           Hoyt, Tom. "Basic Fantasy Role-Playing Game Beginner's Essentials."
+          Release 17. Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
+        </List.Item>
+        <List.Item>
+          Ludlum, et al. "The Basic Fantasy Equipment Emporium." Release 33.
           Basic Fantasy Role-Playing Game, 2023,
           basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Ludlum, et al. "The Basic Fantasy Equipment Emporium." Basic Fantasy
-          Role-Playing Game, 2023, basicfantasy.org/downloads.html.
+          Medders, et al. "Assassins: A Basic Fantasy Supplement." Release 6.
+          Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Medders, et al. "Assassins: A Basic Fantasy Supplement." Basic Fantasy
-          Role-Playing Game, 2023, basicfantasy.org/downloads.html.
+          Castellani, Luigi. "Barbarians: A Basic Fantasy Supplement." Release
+          6. Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Castellani, Luigi. "Barbarians: A Basic Fantasy Supplement." Basic
-          Fantasy Role-Playing Game, 2023, basicfantasy.org/downloads.html.
+          Gonnerman, et al. "Druids: A Basic Fantasy Supplement." Release 12.
+          Basic Fantasy Role-Playing Game, 2022,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Gonnerman, et al. "Druids: A Basic Fantasy Supplement." Basic Fantasy
-          Role-Playing Game, 2022, basicfantasy.org/downloads.html.
+          Gonnerman, et al. "Illusionists: A Basic Fantasy Supplement." Release
+          9. Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Gonnerman, et al. "Illusionists: A Basic Fantasy Supplement." Basic
-          Fantasy Role-Playing Game, 2023, basicfantasy.org/downloads.html.
+          Gonnerman, Chris. "Gnomes: A Basic Fantasy Supplement." Release 7.
+          Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Gonnerman, Chris. "Gnomes: A Basic Fantasy Supplement." Basic Fantasy
-          Role-Playing Game, 2023, basicfantasy.org/downloads.html.
+          Gonnerman, Chris. "Half Humans: A Basic Fantasy Supplement." Release
+          5. Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Gonnerman, Chris. "Half Humans: A Basic Fantasy Supplement." Basic
-          Fantasy Role-Playing Game, 2023, basicfantasy.org/downloads.html.
-        </List.Item>
-        <List.Item>
-          Smoot, et al. "Necromancers: A Basic Fantasy Supplement." Basic
-          Fantasy Role-Playing Game, 2021, basicfantasy.org/downloads.html.
+          Smoot, et al. "Necromancers: A Basic Fantasy Supplement." Release 11.
+          Basic Fantasy Role-Playing Game, 2023,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
           Gonnerman, et al. "Rangers and Paladins: A Basic Fantasy Supplement."
-          Basic Fantasy Role-Playing Game, 2021,
+          Release 4. Basic Fantasy Role-Playing Game, 2021,
           basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Brentlinger, Jason. "Scouts: A Basic Fantasy RPG Supplement." Basic
-          Fantasy Role-Playing Game, 2018, basicfantasy.org/downloads.html.
+          Brentlinger, Jason. "Scouts: A Basic Fantasy RPG Supplement." Release
+          4. Basic Fantasy Role-Playing Game, 2018,
+          basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
           Castellani, et al. "Spellcrafters: A Basic Fantasy RPG Supplement."
-          Basic Fantasy Role-Playing Game, 2023,
+          Release 11. Basic Fantasy Role-Playing Game, 2023,
           basicfantasy.org/downloads.html.
         </List.Item>
         <List.Item>
-          Smoot, R. Kevin. "New Races: A Basic Fantasy RPG Supplement." Basic
-          Fantasy Role-Playing Game, 2018, basicfantasy.org/downloads.html.
+          Smoot, R. Kevin. "New Races: A Basic Fantasy RPG Supplement." Release
+          2. Basic Fantasy Role-Playing Game, 2018,
+          basicfantasy.org/downloads.html.
         </List.Item>
       </List>
     </div>
