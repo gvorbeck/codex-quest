09abde4b54712b7fe3395898d037bcadef007755
Garrett Vorbeck <gvorbeck@gmail.com>
2024-03-11 22:17:54 -0400

    Remove unused imports and buttons from Equipment component

--- src/components/PageCharacterSheet/Equipment/Equipment.tsx
+++ src/components/PageCharacterSheet/Equipment/Equipment.tsx
@@ -1,13 +1,9 @@
-import { Button, Flex } from "antd";
+import { Flex } from "antd";
 import React from "react";
 import CollapseEquipment from "../CollapseEquipment/CollapseEquipment";
 import EquipmentItemDescription from "../CollapseEquipment/EquipmentItemDescription/EquipmentItemDescription";
 import { kickItem, punchItem } from "@/support/equipmentSupport";
-import ModalCustomEquipment from "@/components/ModalCustomEquipment/ModalCustomEquipment";
-import StepEquipment from "@/components/PageNewCharacter/StepEquipment/StepEquipment";
 import { CharacterDataContext } from "@/contexts/CharacterContext";
-import { useDeviceType } from "@/hooks/useDeviceType";
-import classNames from "classnames";
 
 interface EquipmentProps {
   setModalIsOpen: (modalIsOpen: boolean) => void;
@@ -18,54 +14,10 @@ interface EquipmentProps {
 const Equipment: React.FC<
   EquipmentProps & React.ComponentPropsWithRef<"div">
 > = ({ className, setModalIsOpen, setModalTitle, setModalContent }) => {
-  const { isMobile } = useDeviceType();
   const { character, setCharacter, userIsOwner } =
     React.useContext(CharacterDataContext);
-  const handleEditEquipmentClick = () => {
-    setModalIsOpen(true);
-    setModalTitle("Add/Edit Equipment");
-    setModalContent(
-      <StepEquipment
-        character={character}
-        setCharacter={setCharacter}
-        hideDiceButton
-        hideInventory
-      />,
-    );
-  };
-  const handleCustomEquipmentClick = () => {
-    setModalIsOpen(true);
-    setModalTitle("Add Custom Equipment");
-    setModalContent(
-      <ModalCustomEquipment
-        character={character}
-        setCharacter={setCharacter}
-        setModalIsOpen={setModalIsOpen}
-      />,
-    );
-  };
-  const addEquipmentClassNames = classNames(
-    { "w-1/2": !isMobile },
-    "flex-grow",
-  );
   return (
     <Flex vertical gap={16} className={className}>
-      <Flex gap={16} vertical={isMobile}>
-        <Button
-          disabled={!userIsOwner}
-          onClick={handleEditEquipmentClick}
-          className={addEquipmentClassNames}
-        >
-          Add/Edit Equipment
-        </Button>
-        <Button
-          disabled={!userIsOwner}
-          onClick={handleCustomEquipmentClick}
-          className={addEquipmentClassNames}
-        >
-          Add Custom Equipment
-        </Button>
-      </Flex>
       <Flex gap={16} vertical>
         <EquipmentItemDescription
           item={kickItem}
