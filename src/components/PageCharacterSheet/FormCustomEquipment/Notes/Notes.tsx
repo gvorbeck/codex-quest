37d4cff166c29153cafea6189bb5b8ad44401210
Garrett Vorbeck <gvorbeck@gmail.com>
2024-03-29 00:01:15 -0400

    Increase maxLength of Notes TextArea

--- src/components/PageCharacterSheet/FormCustomEquipment/Notes/Notes.tsx
+++ src/components/PageCharacterSheet/FormCustomEquipment/Notes/Notes.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Notes: React.FC<React.ComponentPropsWithRef<"div">> = ({ className }) => {
   return (
     <Form.Item label="Notes" name="notes" className={className}>
-      <Input.TextArea rows={5} />
+      <Input.TextArea maxLength={10000} rows={5} />
     </Form.Item>
   );
 };
