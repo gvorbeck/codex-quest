31c0098f0e3f7197d27bc177260b05a821601b67
Garrett Vorbeck <gvorbeck@gmail.com>
2024-05-02 10:46:02 -0400

    change props

--- src/components/PageNewCharacter/SupplementalContentSwitch/SupplementalContentSwitch.tsx
+++ src/components/PageNewCharacter/SupplementalContentSwitch/SupplementalContentSwitch.tsx
@@ -2,22 +2,19 @@ import { Flex, Switch, Typography } from "antd";
 
 interface SupplementalContentSwitchProps {
   supplementalSwitch: boolean;
-  setSupplementalSwitch: React.Dispatch<React.SetStateAction<boolean>>;
+  onChange: (value: boolean) => void;
+  label?: string;
 }
 
 const SupplementalContentSwitch: React.FC<
   SupplementalContentSwitchProps & React.ComponentPropsWithRef<"div">
-> = ({ supplementalSwitch, setSupplementalSwitch }) => {
-  function handleSupplementalSwitchChange() {
-    setSupplementalSwitch((prevSupplementalSwitch) => !prevSupplementalSwitch);
-  }
+> = ({ supplementalSwitch, onChange, label }) => {
   return (
-    <Flex gap={16}>
-      <Typography.Text>Enable Supplemental Content</Typography.Text>
-      <Switch
-        checked={supplementalSwitch}
-        onChange={handleSupplementalSwitchChange}
-      />
+    <Flex gap={8}>
+      <Typography.Text>
+        {label ?? "Enable Supplemental Content"}
+      </Typography.Text>
+      <Switch checked={supplementalSwitch} onChange={onChange} />
     </Flex>
   );
 };
