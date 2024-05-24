e8e7f43b9655ed53b47146da36a43e2bf589a696
Garrett Vorbeck <gvorbeck@gmail.com>
2024-05-24 13:40:46 -0400

    Add Treasure Generator modal to Hero component

--- src/components/PageGameSheet/Hero/Hero.tsx
+++ src/components/PageGameSheet/Hero/Hero.tsx
@@ -12,6 +12,10 @@ import AddPlayerForm from "../AddPlayerForm/AddPlayerForm";
 import { HourglassOutlined, TeamOutlined } from "@ant-design/icons";
 import { useDeviceType } from "@/hooks/useDeviceType";
 import { breadcrumbItems } from "@/support/cqSupportGeneral";
+import CoinsSvg from "@/assets/svg/CoinsSvg";
+import ModalContainer from "@/components/ModalContainer/ModalContainer";
+import { useModal } from "@/hooks/useModal";
+import TreasureGenerator from "../TreasureGenerator/TreasureGenerator";
 
 interface HeroProps {
   handlePlayersSwitch: (checked: boolean) => void;
@@ -25,35 +29,57 @@ const Hero: React.FC<HeroProps> = ({
   name,
 }) => {
   const { isMobile } = useDeviceType();
+  const { modalDisplay, setModalDisplay, modalOkRef } = useModal();
+
+  function handleTreasureModalClick() {
+    setModalDisplay({
+      isOpen: true,
+      title: "Treasure Generator",
+      content: <TreasureGenerator />,
+    });
+  }
 
   return (
-    <header>
-      <Breadcrumb items={breadcrumbItems(name, TeamOutlined)} />
-      <Typography.Title
-        level={2}
-        className="my-5 font-enchant text-5xl tracking-wide text-center"
-      >
-        {name}
-      </Typography.Title>
-      <Divider className="my-2" />
-      <Flex
-        vertical={isMobile}
-        align={isMobile ? "flex-start" : "center"}
-        gap={8}
-      >
-        <Flex gap={8} align="center">
-          <Typography.Text>Hide PCs</Typography.Text>
-          <Switch className="mr-2" onChange={handlePlayersSwitch} />
-          <Tooltip title="Open Round Tracker">
-            <Button
-              icon={<HourglassOutlined />}
-              onClick={handleRoundTrackerOpen}
-            />
-          </Tooltip>
+    <>
+      <header>
+        <Breadcrumb items={breadcrumbItems(name, TeamOutlined)} />
+        <Typography.Title
+          level={2}
+          className="my-5 font-enchant text-5xl tracking-wide text-center"
+        >
+          {name}
+        </Typography.Title>
+        <Divider className="my-2" />
+        <Flex
+          vertical={isMobile}
+          align={isMobile ? "flex-start" : "center"}
+          gap={8}
+        >
+          <Flex gap={8} align="center">
+            <Typography.Text>Hide PCs</Typography.Text>
+            <Switch className="mr-2" onChange={handlePlayersSwitch} />
+            <Tooltip title="Open Round Tracker">
+              <Button
+                icon={<HourglassOutlined />}
+                onClick={handleRoundTrackerOpen}
+              />
+            </Tooltip>
+            <Tooltip title="Open Treasure Generator">
+              <Button
+                icon={<CoinsSvg className="w-6 fill-shipGray" />}
+                onClick={handleTreasureModalClick}
+              />
+            </Tooltip>
+          </Flex>
+          <AddPlayerForm className="mb-4 flex-grow" />
         </Flex>
-        <AddPlayerForm className="mb-4 flex-grow" />
-      </Flex>
-    </header>
+      </header>
+      <ModalContainer
+        modalDisplay={modalDisplay}
+        setModalDisplay={setModalDisplay}
+        modalOk={modalOkRef.current}
+      />
+    </>
   );
 };
 
