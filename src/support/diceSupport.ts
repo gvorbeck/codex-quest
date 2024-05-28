2c1d02930f8951f47a800241c3bc0c5e71851d61
Garrett Vorbeck <gvorbeck@gmail.com>
2024-05-28 08:34:09 -0400

    Add loot generation for different levels

--- src/support/diceSupport.ts
+++ src/support/diceSupport.ts
@@ -87,8 +87,53 @@ export function getLoot(level: number | string): Loot {
     magicItems: 0,
   };
   const loot = { ...emptyLoot };
+  console.log(level);
 
   switch (level) {
+    case "A":
+      break;
+    case "B":
+      break;
+    case "C":
+      break;
+    case "D":
+      break;
+    case "E":
+      break;
+    case "F":
+      break;
+    case "G":
+      break;
+    case "H":
+      break;
+    case "I":
+      break;
+    case "J":
+      break;
+    case "K":
+      break;
+    case "L":
+      break;
+    case "M":
+      break;
+    case "N":
+      break;
+    case "O":
+      break;
+    case "P":
+      break;
+    case "Q":
+      break;
+    case "R":
+      break;
+    case "S":
+      break;
+    case "T":
+      break;
+    case "U":
+      break;
+    case "V":
+      break;
     case 1:
       chances.copper = [0.75, 8, 1];
       chances.silver = [0.5, 6, 1];
