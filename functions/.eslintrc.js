fa091389449207a4ac39e8e6c7bb539960a026d5
Garrett Vorbeck <gvorbeck@gmail.com>
2024-06-12 09:34:29 -0400

    Update ESLint configuration

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -21,13 +21,12 @@ module.exports = {
     "/lib/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
+    "object-curly-spacing": "off",
+    "quote-props": "off",
   },
 };
