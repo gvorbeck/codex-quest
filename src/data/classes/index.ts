1b861e51d6404c8bda7b5ba0353c9e9aa501bbfb
Garrett Vorbeck <garrett.vorbeck@anywhere.re>
2025-08-19 15:04:46 -0400

    Add Assassin class with attributes and update class exports

--- src/data/classes/index.ts
+++ src/data/classes/index.ts
@@ -2,10 +2,12 @@ export { cleric } from "./cleric";
 export { fighter } from "./fighter";
 export { magicUser } from "./magic-user";
 export { thief } from "./thief";
+export { assassin } from "./assassin";
 
 import { cleric } from "./cleric";
 import { fighter } from "./fighter";
 import { magicUser } from "./magic-user";
 import { thief } from "./thief";
+import { assassin } from "./assassin";
 
-export const allClasses = [cleric, fighter, magicUser, thief];
+export const allClasses = [cleric, fighter, magicUser, thief, assassin];
