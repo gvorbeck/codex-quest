1e8a9331b41efa9e2f3f79af61c68f18f9c5b3a8
Garrett Vorbeck <garrett.vorbeck@anywhere.re>
2025-08-19 14:55:35 -0400

    Refactor class exports in index.ts to consolidate imports for character classes

--- src/data/classes/index.ts
+++ src/data/classes/index.ts
@@ -3,4 +3,9 @@ export { fighter } from "./fighter";
 export { magicUser } from "./magic-user";
 export { thief } from "./thief";
 
+import { cleric } from "./cleric";
+import { fighter } from "./fighter";
+import { magicUser } from "./magic-user";
+import { thief } from "./thief";
+
 export const allClasses = [cleric, fighter, magicUser, thief];
