d4a37d0b9452193ef79153d00fc913847b9e66d9
Garrett Vorbeck <garrett.vorbeck@anywhere.re>
2025-08-19 21:22:18 -0400

    refactor: Remove usePerformanceMonitor hook and clean up exports in index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
-export { useA11y } from "./useA11y";
 export { useCascadeValidation } from "./useCascadeValidation";
 export { useLocalStorage } from "./useLocalStorage";
-export { usePerformanceMonitor } from "./usePerformanceMonitor";
-export { useValidation, useMultiValidation } from "./useValidation";
+export { useValidation } from "./useValidation";
