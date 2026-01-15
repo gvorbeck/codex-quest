ca31878ee36e3fdf58de2d3cc001563f1ddff8d7
J. Garrett Vorbeck <gvorbeck@gmail.com>
2026-01-15 15:53:03 -0500

    feat: Update allowedClasses to include druid for multiple races

--- src/data/races/dwarf.ts
+++ src/data/races/dwarf.ts
@@ -7,7 +7,7 @@ export const dwarf: Race = {
     "Dwarves are typically practical, stubborn and courageous. They can also be introspective, suspicious and possessive.",
   physicalDescription:
     "Dwarves are a short, stocky race; both male and female Dwarves stand around four feet tall and typically weigh around 120 pounds. Their long hair and thick beards are dark brown, gray or black. They take great pride in their beards, sometimes braiding or forking them. They have a fair to ruddy complexion. Dwarves have stout frames and a strong, muscular build. They are rugged and resilient, with the capacity to endure great hardships.",
-  allowedClasses: ["cleric", "fighter", "thief", "barbarian", "paladin"],
+  allowedClasses: ["cleric", "druid", "fighter", "thief", "barbarian", "paladin"],
   abilityRequirements: [
     {
       ability: "constitution",
